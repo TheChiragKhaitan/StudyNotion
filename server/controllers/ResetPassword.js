60524ca6f697bccfc2dce42f6259dbf5efc817c9
Chirag Khaitan <chiragkhaitan2014@gmail.com>
2024-08-14 04:51:42 +0530

    bug fixed

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -36,7 +36,7 @@ exports.resetPasswordToken = async(req, res) => {
                                         );
         //create url
 
-        const url = `http://localhost:3000/update-password/${token}`;
+        const url = `https://studynotion-chirag-khaitan.vercel.app/update-password/${token}`;
 
         //send email
 
